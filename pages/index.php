9a3a37097ad9eb36c27ad88d625aeddf02926162
alfredo marcano <alfredomarcan@gmail.com>
2016-07-18 16:51:27 -0400

    Segunda Fase
    Fase realizada en día

--- pages/index.php
+++ pages/index.php
@@ -4,12 +4,12 @@
     <title>Sistema de Control de Vehículo para Talleres y Seguros</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!--<link rel="stylesheet" href="css/bootstrap-responsive.min.css">-->
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="../css/estilo.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../font-awesome-4.6.3/css/font-awesome.min.css">
+    <script src="../js/bootstrap.min.js"></script>
 </head>
 <body class="" id="back-imagen">
 <div class="login-box">
@@ -39,7 +39,7 @@
             </div>
         </form>
         <div class="social-auth-links text-center">
-            <p>- OR -</p>
+            <br>
             <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Iniciar usando Facebook</a>
             <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Iniciar usuando Google+</a>
         </div>
@@ -47,20 +47,19 @@
         <a href="register.html" class="text-center">Registrate se un miembro</a>
     </div>
 </div>
-<footer class="footer-login" >
+</body>
+<footer class="footer_login" >
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-offset-2 col-sm-1 text-center"><i class="fa fa-twitter fa-3x"></i> Twitter</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-facebook fa-3x"></i> Facebook</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-instagram fa-3x"></i> Instagram</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-youtube-square fa-3x"></i> Youtube</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-skype fa-3x"></i> Skype</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-snapchat fa-3x"></i> Snapchat</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-linkedin fa-3x"></i> Linkedin</div>
-            <div class="col-sm-1 text-center"><i class="fa fa-google fa-3x"></i> Google</div>
+            <div class="col-sm-offset-2 col-sm-1 text-center"><i class="fa fa-twitter fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-facebook fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-instagram fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-youtube-square fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-skype fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-snapchat fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-linkedin fa-3x"></i></div>
+            <div class="col-sm-1 text-center"><i class="fa fa-google fa-3x"></i></div>
         </div>
     </div>
 </footer>
-
-</body>
 </html>
---
+++ pages/main.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="Es-es" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Sistema de Control de Vehículo para Talleres y Seguros</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="../font-awesome-4.6.3/css/font-awesome.min.css">
+    <script src="../js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar-btn">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Sicovets</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Archivo</a></li>
+                <li><a href="#">Edicion</a></li>
+                <li><a href="#">Ejecución</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="index.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+        <div class="container">
+                <ul>
+                    <li>
+                        <a class="current" href="index.html"><span aria-hidden="true" class="hightop-home"></span>Dashboard</a>
+                    </li>
+                    <li><a href="social.html">
+                            <span aria-hidden="true" class="hightop-feed"></span>Social Feed</a>
+                    </li>
+                    <li class="dropdown"><a data-toggle="dropdown" href="#">
+                            <span aria-hidden="true" class="hightop-star"></span>UI Features<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="buttons.html">Buttons</a>
+                            </li>
+                            <li>
+                                <a href="fontawesome.html">Font Awesome Icons</a>
+                            </li>
+                            <li>
+                                <a href="glyphicons.html">Glyphicons</a>
+                            </li>
+                            <li>
+                                <a href="components.php">Components</a>
+                            </li>
+                            <li>
+                                <a href="widgets.html">Widgets</a>
+                            </li>
+                            <li>
+                                <a href="nestable-lists.html">Nestable Lists</a>
+                            </li>
+                            <li>
+                                <a href="typo.html">Typography</a>
+                            </li>
+                            <li>
+                                <a href="grid.html">Grid Layout</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a data-toggle="dropdown" href="#">
+                            <span aria-hidden="true" class="hightop-forms"></span>Forms<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="form-components.html">Form Components</a>
+                            </li>
+                            <li>
+                                <a href="form-advanced.html">Advanced Forms</a>
+                            </li>
+                            <li>
+                                <a href="xeditable.html">X-Editable</a>
+                            </li>
+                            <li>
+                                <a href="file-upload.html">Multiple File Upload</a>
+                            </li>
+                            <li>
+                                <a href="dropzone-file-upload.html">Dropzone File Upload</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a data-toggle="dropdown" href="#">
+                            <span aria-hidden="true" class="hightop-tables"></span>Tables<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="tables.html">Basic tables</a>
+                            </li>
+                            <li>
+                                <a href="datatables.html">DataTables</a>
+                            </li>
+                            <li>
+                                <a href="datatables-editable.html">Editable DataTables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="charts.html">
+                            <span aria-hidden="true" class="hightop-charts"></span>Charts</a>
+                    </li>
+                    <li class="dropdown open"><a data-toggle="dropdown" href="#">
+                            <span aria-hidden="true" class="hightop-pages"></span>Pages<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="chat.html">Chat</a>
+                            </li>
+                            <li>
+                                <a href="calendar.html">Calendar</a>
+                            </li>
+                            <li>
+                                <a href="timeline.html">Timeline</a>
+                            </li>
+                            <li>
+                                <a href="login1.html">Login 1</a>
+                            </li>
+                            <li>
+                                <a href="login2.html">Login 2</a>
+                            </li>
+                            <li>
+                                <a href="messages.html">Messages/Inbox</a>
+                            </li>
+                            <li>
+                                <a href="pricing.html">Pricing Tables</a>
+                            </li>
+                            <li>
+                                <a href="signup1.html">Sign Up 1</a>
+                            </li>
+                            <li>
+                                <a href="signup2.html">Sign Up 2</a>
+                            </li>
+                            <li>
+                                <a href="invoice.html">Invoice</a>
+                            </li>
+                            <li>
+                                <a href="faq.html">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="filters.html">Filter Results</a>
+                            </li>
+                            <li>
+                                <a href="404-page.html">404 Page</a>
+                            </li>
+                            <li>
+                                <a href="500-page.html">500 Error</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="gallery.html">
+                            <span aria-hidden="true" class="hightop-gallery"></span>Gallery</a>
+                    </li>
+                </ul>
+
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row content">
+            <section class="content-siderbar">
+                    <h1>Control de estadistica</h1>
+                    <!-- Lista de rutas--->
+                    <ol class="breadcrumb">
+                        <li class="active"><i class="fa fa-dashboard"></i> <a href="main.php">Dashboard</a></li>
+                        <!--<li><i class="fa fa-bar-chart-o"></i> Control</li>-->
+                    </ol>
+                <hr>
+
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-car fa-3x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">26</div>
+                                        <div>Vehiculos</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Ver Detalles</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-archive fa-3x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">12</div>
+                                        <div>Archivos</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Ver Detalles</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-opencart fa-3x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">124</div>
+                                        <div>Ordenes</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Ver Detalles</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-fire fa-3x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">13</div>
+                                        <div>Retrasos</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Ver Detalles</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <!---------
+                <hr>
+                <h2>I Love Food</h2>
+                <h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
+                <h5><span class="label label-danger">Food</span> <span class="label label-primary">Ipsum</span></h5><br>
+                <p>Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <br><br>
+
+                <h4><small>RECENT POSTS</small></h4>
+                <hr>
+                <h2>Officially Blogging</h2>
+                <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
+                <h5><span class="label label-success">Lorem</span></h5><br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <hr>
+
+                <h4>Leave a Comment:</h4>
+                <form role="form">
+                    <div class="form-group">
+                        <textarea class="form-control" rows="3" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </form>
+                <br><br>
+                 ------->
+                </section>
+            </div>
+            </section>
+        </div>
+    </div>
+</body>
+<footer id="footer" >
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-sm-3 text-center"><i class="fa fa-twitter fa-3x"></i><br>Twitter</div>
+            <div class="col-sm-3 text-center"><i class="fa fa-facebook fa-3x"></i><br>Facebook</div>
+            <div class="col-sm-3 text-center"><i class="fa fa-instagram fa-3x"></i><br>Instagram</div>
+            <div class="col-sm-3 text-center"><i class="fa fa-github fa-3x"></i></br>Github</div>
+        </div><br>
+        <!-------
+        <div class="row">
+            <div class="col-sm-3"><img src="imagen/Chevrolet.png" class="img-responsive" style="width:200px; height:100px;/"></div>
+            <div class="col-sm-3"><img src="imagen/Ford.png" class="img-responsive" style="width:200px; height:100px;"/></div>
+            <div class="col-sm-3"><img src="imagen/Toyota.png" class="img-responsive" style="width:200px; height:100px;"/></div>
+            <div class="col-sm-3"><img src="imagen/jeep.png" class="img-responsive" style="width:200px; height:100px;"/></div>
+        </div> ------->
+    </div>
+</footer>
+</html>
+
+
+
+
+
+
+
